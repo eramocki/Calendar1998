58a1f50c2cd4b227070374008a3c6cd249ce5c11
Josh Baird <jjb.pub01@gmail.com>
2018-02-28 21:55:21 -0500

    Add hello world idea project

---
+++ src/main/java/edu/oakland/Main.java
@@ -0,0 +1,10 @@
+package edu.oakland;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("What shall we use to fill the empty spaces?");
+    }
+
+}
