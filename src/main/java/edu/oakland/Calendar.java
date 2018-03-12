c174a62014d3a1adb3637ec8e044c4ab15edd550
seanramocki <sramocki@oakland.edu>
2018-03-12 11:53:37 -0400

    Calendar class template

---
+++ src/main/java/edu/oakland/Calendar.java
@@ -0,0 +1,10 @@
+package edu.oakland;
+
+public class Calendar {
+
+    public void displayEvent(Event event) {}
+    public void addEvent(Event event){}
+    public void removeEvent(Event event){}
+    public void updateEvent(Event event){}
+
+}
